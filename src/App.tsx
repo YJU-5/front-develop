b57938880bf317052c828e640dc6fe8ee7d9f3e4
jinmo550 <eorn8110@g.yju.ac.kr>
2024-12-13 14:47:29 +0900

    taillwindcss 설치치

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,14 @@ import BulletinBoard from "./pages/bulletinBoard/BulletinBoard";
 import DetailedPost from "./pages/bulletinBoard/DetailedPost";
 import NewPost from "./pages/bulletinBoard/NewPost";
 import Introduction from "./pages/Introduction";
-import LocalSemester from "./pages/LocalSemester";
+import LocalSemester from "./pages/LocalSemester/LocalSemester";
 import CreateMember from "./pages/introduction/CreateMember";
+import IntroductionDescription from "./pages/introduction/IntroductionDescription";
+import Semester_create from "./pages/LocalSemester/Semester_create";
+import Detailed_Page from "./pages/LocalSemester/Detailed_Page";
+import Update_Page from "./pages/LocalSemester/Update_Page";
+import Login from "./pages/Login/Login";
+import Sign_up from "./pages/Login/Sign_up";
 
 function App() {
 
@@ -29,7 +35,15 @@ function App() {
             element={<IntroductionDescription />}
           ></Route>
           <Route path="/create-member" element={<CreateMember />} />
+          
+          {/* 현지학기 */}
           <Route path="/localSemester" element={<LocalSemester />} />
+          <Route path="/Semester-create" element={<Semester_create />} />
+            <Route path="/Detailed_Page" element={<Detailed_Page />} />
+            <Route path="/Update_Page" element={<Update_Page />} />
+           {/* 로그인 */}
+           <Route path="/login" element={<Login />} />
+            <Route path="/Sign_up" element={<Sign_up />} />
         </Routes>
       </Router>
     </div>
